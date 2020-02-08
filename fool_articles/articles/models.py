db30535d1a0b722b9c1379e2c02c85f988ee8646
wwwidmer <wwwidmer@protonmail.com>
2020-02-08 16:15:03 -0700

    Environment setup (Templates / Static / Env)
    Allow per environment setup (.env file)
    
    Add autopep8
    
    Add templates dir + base templates
    
    Add static + base for js and css
    
    Add `articles` app, a place where class + api routes will exist

---
+++ fool_articles/articles/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+from models.article import Article
--- fool_articles/manage.py
+++ fool_articles/manage.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+import env_file
 import os
 import sys
 
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fool_articles.settings')
+    env_file.load()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
