021345dee8f0b6b9b25bc656c890bbbbb163040d
wwwidmer <wwwidmer@protonmail.com>
2020-02-11 20:29:27 -0700

    Single article page view + template
    Still ugly! Routes! Views! Tests!

--- fool_articles/articles/test_views.py
+++ fool_articles/articles/test_views.py
@@ -11,10 +11,11 @@ def test_index(rf):
 
 
 def test_article_view(rf):
-    request = rf.get("/article")
+    request = rf.get("/article", HTTP_ACCEPT='application/json')
+
     view = ArticleView()
     view.kwargs = {}
-    response = view.get(request)
+    response = view.get(request, HTTP_ACCEPT='application/json')
     content = json.loads(response.content)
 
     assert 'articles' in content
@@ -23,7 +24,8 @@ def test_article_view(rf):
 
 
 def test_article_view_slug(rf):
-    request = rf.get("/article/10-promise")
+    request = rf.get("/article/10-promise", HTTP_ACCEPT='application/json')
+
     view = ArticleView()
     view.kwargs = {
         'slug': '10-promise'
@@ -34,3 +36,32 @@ def test_article_view_slug(rf):
     assert 'articles' in content
     assert len(content['articles']) == 1
     assert content['articles'][0] is not None
+
+
+def test_article_view_uuid(rf):
+    request = rf.get("/article/10-promise", HTTP_ACCEPT='application/json')
+    view = ArticleView()
+    view.kwargs = {
+        'uuid': 'd6397ee8-c4da-11e7-a496-0050569d4be0'
+    }
+    response = view.get(request)
+
+    content = json.loads(response.content)
+
+    assert 'articles' in content
+    assert len(content['articles']) == 1
+    assert content['articles'][0] is not None
+
+
+def test_article_return_template(rf):
+    request = rf.get("/article/10-promise")
+    view = ArticleView()
+    view.kwargs = {
+        'slug': '10-promise'
+    }
+    response = view.get(request)
+
+    assert response._headers['content-type'] == (
+        'Content-Type', 'text/html; charset=utf-8'
+    )
+    assert '<!DOCTYPE html>' in str(response.content)
--- fool_articles/articles/views.py
+++ fool_articles/articles/views.py
@@ -11,16 +11,24 @@ def index(request):
 
     context = {
         "headline_article": headliners[0],
-        "articles": ArticleManager.get_articles()
+        "articles": ArticleManager.get_articles()[:3]
     }
     return render(request, 'index.html', context)
 
 
 class ArticleView(View):
-    def get(self, request):
+    def get(self, request, **kwargs):
         articles = ArticleManager.get_articles(
-            slug=self.kwargs.get('slug')
+            slug=self.kwargs.get('slug'),
+            uuid=self.kwargs.get('uuid')
         )
-        return JsonResponse({
-            "articles": [serialize_article(article) for article in articles]
-        })
+        context = {
+            "articles": [
+                serialize_article(article) for article in articles
+            ]
+        }
+
+        if request.META.get('HTTP_ACCEPT') == 'application/json':
+            return JsonResponse(context)
+
+        return render(request, 'article.html', context)
