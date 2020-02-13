13c7a1951193ce5b35b22b66eab7b3d3c194409d
wwwidmer <wwwidmer@protonmail.com>
2020-02-13 11:39:27 -0700

    Add a form for Comments and alter schema to allow for nully usernames

--- fool_articles/articles/models/comment.py
+++ fool_articles/articles/models/comment.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+
 class Comment(models.Model):
-    created_at = models.DateTimeField(null=False, auto_now_add=True, blank=True)
-    username = models.CharField(max_length=255)
+    created_at = models.DateTimeField(
+        null=False, auto_now_add=True, blank=True
+    )
+    username = models.CharField(
+        max_length=255, blank=True, null=True
+    )
     text = models.TextField(null=False)
 
     article_uuid = models.CharField(max_length=255, null=False)
-
